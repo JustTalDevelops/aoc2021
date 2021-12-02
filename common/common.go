5beb2334fb1d7e9339c7da281813b72db8db073d
JustTal <justtal@protonmail.com>
2021-12-02 09:34:12 -0600

    Simplified part two answer

--- common/common.go
+++ common/common.go
@@ -14,26 +14,36 @@ func Inputs() []string {
 	}
 	inputs := strings.Split(string(b), "\n")
 	for i := 0; i < len(inputs); i++ {
-        inputs[i] = strings.TrimSpace(inputs[i])
-    }
+		inputs[i] = strings.TrimSpace(inputs[i])
+	}
 	return inputs
+}
+
+// InputsSplit returns the input.txt file as a [][]string.
+func InputsSplit(s string) [][]string {
+	inputs := Inputs()
+	result := make([][]string, len(inputs))
+	for i := 0; i < len(inputs); i++ {
+		result[i] = strings.Split(inputs[i], s)
+	}
+	return result
 }
 
 // InputsAsInts reads the input.txt file and returns it as a []int.
 func InputsAsInts() []int {
-    inputs := Inputs()
-    ints := make([]int, len(inputs))
-    for i, input := range inputs {
-        ints[i] = Atoi(input)
-    }
-    return ints
+	inputs := Inputs()
+	ints := make([]int, len(inputs))
+	for i, input := range inputs {
+		ints[i] = Atoi(input)
+	}
+	return ints
 }
 
 // Atoi converts a string to an int, otherwise it panics.
 func Atoi(s string) int {
-    i, err := strconv.Atoi(s)
-    if err != nil {
-        panic(err)
-    }
-    return i
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
--- days/two/main.go
+++ days/two/main.go
@@ -3,42 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/justtaldevelops/aoc2021/common"
-	"strings"
 )
 
 func main() {
-	input := common.Inputs()
+	input := common.InputsSplit(" ")
 
-	var horizontalPosition, depth, aim int
+	var positionOne, positionTwo, depthOne, depthTwo, aim int
 	for _, in := range input {
-		s := strings.Split(in, " ")
-		command, change := s[0], common.Atoi(s[1])
+		command, change := in[0], common.Atoi(in[1])
 
 		switch command {
 		case "forward":
-			horizontalPosition += change
+			positionOne += change
+			positionTwo += change
+			depthTwo += aim * change
 		case "down":
-			depth += change
-		case "up":
-			depth -= change
-		}
-	}
-	fmt.Println("Part One Answer:", horizontalPosition*depth)
-	horizontalPosition, depth = 0, 0
-
-	for _, in := range input {
-		s := strings.Split(in, " ")
-		command, change := s[0], common.Atoi(s[1])
-
-		switch command {
-		case "down":
+			depthOne += change
 			aim += change
 		case "up":
+			depthOne -= change
 			aim -= change
-		case "forward":
-			horizontalPosition += change
-			depth += aim * change
 		}
 	}
-	fmt.Println("Part Two Answer:", horizontalPosition*depth)
+	fmt.Println("Part One Answer:", positionOne*depthOne)
+	fmt.Println("Part Two Answer:", positionTwo*depthTwo)
 }
