9480b7e6fbe612119c37c57be2118f11a360ed70
Tal <justtal@protonmail.com>
2021-12-06 08:52:07 -0600

    Update main.go

--- days/six/main.go
+++ days/six/main.go
@@ -20,14 +20,13 @@ func main() {
 func population(populations []int, days int) int {
 	// Tick each day.
 	for i := 0; i < days; i++ {
-		// Current population.
-		currentPopulation := populations[0]
-
 		// Tick down a day.
 		for j := 0; j < len(populations)-1; j++ {
 			populations[j] = populations[j+1]
 		}
 
+		// Current population.
+		currentPopulation := populations[0]
 		// Add the new current population.
 		populations[8] = currentPopulation
 		// Reset the current population.
